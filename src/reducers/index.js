0cf37e90e534475f8727d5f93c1f37eb41013388
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-10-22 15:52:58 +0900

    refactor: Reducer を分割

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,49 +1,8 @@
-import * as actionTypes from "../utils/actionTypes";
+import { combineReducers } from "redux";
+import calculator from "./calculator";
 
-const initialState = {
-  inputValue: 0,
-  resultValue: 0,
-  operator: "",
-  isCalculate: false,
-  isShowResult: false,
-};
+const reducer = combineReducers({
+  calculator,
+});
 
-const calculator = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.INPUT_NUMBER:
-      return {
-        ...state,
-        inputValue: action.number,
-      };
-
-    case actionTypes.PLUS:
-      return {
-        ...state,
-        resultValue: state.resultValue + state.inputValue,
-        operator: "+",
-      };
-    case actionTypes.MINUS:
-      return {
-        ...state,
-        resultValue: state.resultValue - state.inputValue,
-        operator: "-",
-      };
-    case actionTypes.MULTIPLY:
-      return {
-        ...state,
-        resultValue: state.resultValue * state.inputValue,
-        operator: "*",
-      };
-    case actionTypes.DIVIDE:
-      return {
-        ...state,
-        resultValue: state.resultValue / state.inputValue,
-        operator: "/",
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default calculator;
+export default reducer;
