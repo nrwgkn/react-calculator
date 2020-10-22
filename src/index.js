ce4a876330a73a9944c675eb69579321094805c1
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-10-22 15:42:55 +0900

    feat: Store の追加

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import CalculatorContainer from "./containers/CalculatorContainer";
+
+const store = createStore(reducer);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <CalculatorContainer />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
