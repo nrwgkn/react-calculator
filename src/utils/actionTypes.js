54fdf9bc9f92ca84f3a0e407dc983557433c610f
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-10-22 14:08:24 +0900

    feat: 各ボタンの Action Typesを指定

---
+++ src/utils/actionTypes.js
@@ -0,0 +1,10 @@
+// 数値入力
+export const INPUT_NUMBER = "INPUT_NUMBER";
+// 演算子入力
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+// イコール、クリア入力
+export const EQUAL = "EQUAL";
+export const ALL_CLEAR = "ALL_CLEAR";
